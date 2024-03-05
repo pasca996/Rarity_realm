bc6e9ed968db04e1115f794bc709f66a7878f8ff
Andrea Pascarella <andrea.pascarella@outlook.it>
2024-03-05 19:45:37 +0100

    aggiunto il login

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -16,6 +16,21 @@
     <link rel="stylesheet" href="owlcarousel\assets\owl.carousel.css">
     <link rel="stylesheet" href="owlcarousel\assets\owl.theme.default.css">
     <link rel="stylesheet" href="owlcarousel\assets\owl.theme.green.css">
+    <!-- Font Awesome -->
+<link
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css"
+rel="stylesheet"
+/>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -37,6 +52,10 @@
     <script src="owlcarousel/owl.carousel.min.js"></script>
     <!-- Contact Javascript File -->
     {{-- <script src="mail/jqBootstrapValidation.min.js"></script> --}}
+    <script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.umd.min.js"
+></script>
 </body>
 
 </html>
