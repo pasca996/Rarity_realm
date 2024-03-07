03e5c4e98ad0d626d91bda2678edd3f1c751557f
Vladut Alexandru Ciobotaru <ciobotarualexandru00@gmail.com>
2024-03-07 09:39:56 +0100

    aggiunti annunci dinamici in home

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Announcement;
+use App\Models\Category;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -13,10 +14,9 @@ class Controller extends BaseController
     use AuthorizesRequests, ValidatesRequests;
 
     public function welcome() {
-        // $announcements = Announcement::take(6)->get()->orderBy('created_at' , 'desc');
-        $announcements = DB::table('announcements')
-                ->orderBy('created_at', 'desc')
-                ->get();
+        
+        $announcements = Announcement::orderBy('created_at', 'desc')->take(8)->get();
+
         return view('welcome' , compact('announcements'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -157,7 +157,8 @@
                                 </div>
                             </div>
                             <div class="text-center py-4">
-                                <a class="h6 text-decoration-none text-truncate" href="">{{$announcement->title}}</a>
+                                <a class="h4 text-decoration-none text-truncate" href="">{{$announcement->title}}</a>
+                                <br>
                                 <a class="h6 text-decoration-none text-truncate" href="">{{$announcement->category->name}}</a>
                                 <div class="d-flex align-items-center justify-content-center mt-2">
                                     <h5>${{$announcement->price}}</h5>
