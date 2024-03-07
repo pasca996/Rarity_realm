3b52d016172e25830d86c4afa2a33ed6f62d85ba
Andrea Pascarella <andrea.pascarella@outlook.it>
2024-03-07 09:26:31 +0100

    aggiunte card ciclate

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Announcement;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
     public function welcome() {
-        return view('welcome');
+        // $announcements = Announcement::take(6)->get()->orderBy('created_at' , 'desc');
+        $announcements = DB::table('announcements')
+                ->orderBy('created_at', 'desc')
+                ->get();
+        return view('welcome' , compact('announcements'));
     }
 }
