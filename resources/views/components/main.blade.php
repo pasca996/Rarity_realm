f2594b3e0ed80d6f5b986488730b864e9dd22877
Andrea Pascarella <andrea.pascarella@outlook.it>
2024-03-08 21:02:56 +0100

    update

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -21,20 +21,6 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
     <!-- Font Awesome -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    rel="stylesheet"
-    />
-    <!-- Google Fonts -->
-    <link
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    rel="stylesheet"
-    />
-    <!-- MDB -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css"
-    rel="stylesheet"
-    />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
