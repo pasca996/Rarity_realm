53c9dcb2f0f1c8eed24ec507e549e6482acccaae
Andrea Pascarella <andrea.pascarella@outlook.it>
2024-03-08 20:57:23 +0100

    form update

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -20,6 +20,21 @@
     <link rel="icon" type="image/png" sizes="192x192"  href="/img/logo/icona_192x192.png">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
+    <!-- Font Awesome -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css"
+    rel="stylesheet"
+    />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -33,7 +48,10 @@
     <!-- Back to Top -->
     <a href="#" class="btn btn-success back-to-top"><i class="fa fa-angle-double-up"></i></a>
 
-
+    <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.umd.min.js"
+    ></script>
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="easing/easing.min.js"></script>
