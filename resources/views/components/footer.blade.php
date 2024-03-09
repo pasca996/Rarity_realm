9fdbcad5fa0c597c460e3318e2bdab687e937e2a
Andrea Pascarella <andrea.pascarella@outlook.it>
2024-03-09 11:39:58 +0100

    update bug payment

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -70,7 +70,7 @@
         <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
 
             <div class="col-md-6 px-xl-0 text-center text-md-right">
-                <img class="img-fluid" src="img/payments.png" alt="">
+                <img class="img-fluid" src="/img/payments.png" alt="">
             </div>
         </div>
     </div>
