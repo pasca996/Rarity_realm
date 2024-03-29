1e038ecf18d89ab1ed2f858d36afe196a208945a
Andrea Pascarella <andrea.pascarella@outlook.it>
2024-03-29 18:34:57 +0100

    update

--- resources/views/livewire/user-ads.blade.php
+++ resources/views/livewire/user-ads.blade.php
@@ -12,43 +12,45 @@
                     {{__('ui.ann14')}}
                 </a>
             </div>
-            <table class="table border mt-5 mb-5 shadow-ads">
-                <thead>
-                    <tr>
-                        <th scope="col">{{__('ui.ann32')}}</th>
-                        <th scope="col">{{__('ui.ann31')}}</th>
-                        <th scope="col">{{__('ui.ann19')}}</th>
-                        <th scope="col">{{__('ui.ann17')}}</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody wire:poll.15ms wire:target="delete">
-                    @foreach ($announcements as $announcement)
+            <div class="table-responsive-sm table-responsive-md table-responsive-lg">
+                <table class="table border mt-5 mb-5 shadow-ads">
+                    <thead>
                         <tr>
-                            <th scope="row">
-                                <img class="card-img-top rounded-lg" style="width:9rem" src="{{!$announcement->images()->get()->isEmpty() ? $announcement->images()->first()->getUrl(300,300) :  '\img\logo\logo_con_sfondo_300.png'}}"
-                                alt="..." />
-                            </th>
-                          
-                            <td>{{ $announcement->title }}</td>
-                            <td>{{ $announcement->price }}</td>
-                            <td>{{ $announcement->category->name }}</td>
-                            <td>
-
-                                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                    <a href="{{route('announcement.show',  ['announcement' => $announcement->id])}}" class="btn bg-green shadow-btn searchAnimation mx-2">
-                                        {{__('ui.ann35')}}
-                                    </a>
-                                        <button wire:click="edit({{$announcement}})" type="button" class="btn btnPurple shadow-btn searchAnimation mx-2" data-toggle="modal" data-target="#exampleModal">
-                                            {{__('ui.ann33')}}
-                                        </button>
-                                        <button wire:click="delete({{ $announcement }})" class="btn shadow-btn rounded-lg searchAnimation btn-danger mx-2">{{__('ui.ann34')}}</button>
-                                </div>
-                            </td>
+                            <th scope="col">{{__('ui.ann32')}}</th>
+                            <th scope="col">{{__('ui.ann31')}}</th>
+                            <th scope="col">{{__('ui.ann19')}}</th>
+                            <th scope="col">{{__('ui.ann17')}}</th>
+                            <th scope="col"></th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody wire:poll.15ms wire:target="delete">
+                        @foreach ($announcements as $announcement)
+                            <tr>
+                                <th scope="row">
+                                    <img class="card-img-top rounded-lg" style="width:9rem" src="{{!$announcement->images()->get()->isEmpty() ? $announcement->images()->first()->getUrl(300,300) :  '\img\logo\logo_con_sfondo_300.png'}}"
+                                    alt="..." />
+                                </th>
+                            
+                                <td>{{ $announcement->title }}</td>
+                                <td>{{ $announcement->price }}</td>
+                                <td>{{ $announcement->category->name }}</td>
+                                <td>
+
+                                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                        <a href="{{route('announcement.show',  ['announcement' => $announcement->id])}}" class="btn bg-green shadow-btn searchAnimation mx-2">
+                                            {{__('ui.ann35')}}
+                                        </a>
+                                            <button wire:click="edit({{$announcement}})" type="button" class="btn btnPurple shadow-btn searchAnimation mx-2" data-toggle="modal" data-target="#exampleModal">
+                                                {{__('ui.ann33')}}
+                                            </button>
+                                            <button wire:click="delete({{ $announcement }})" class="btn shadow-btn rounded-lg searchAnimation btn-danger mx-2">{{__('ui.ann34')}}</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
             {{ $announcements->links() }}
             <livewire:form >
         </div>
